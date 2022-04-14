656ead1383f87fce0d9d86bb728f5daf579c51da
Sabina Tarasova <sibipka@gmail.com>
2022-04-14 12:20:14 +0400

    Инфо контроллер, плюс кнопка и алерт

--- Navigation/InfoViewController.swift
+++ Navigation/InfoViewController.swift
@@ -8,22 +8,34 @@
 import UIKit
 
 class InfoViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
+        makebutton()
+        
+    }
+    private func makebutton() {
+        let button = UIButton(frame: CGRect(x: 0, y: 0, width: 150, height: 50))
+        button.backgroundColor = .systemGray6
+        button.center = view.center
+        button.backgroundColor = .black
+        button.setTitle("Назад", for: .normal)
+        button.addTarget(self, action: #selector(tapAction), for: .touchUpInside)
+        view.addSubview(button)
+    }
+    @objc private func tapAction() {
+        let alert = UIAlertController(title: "Вернуться назад", message: "Вы точно хотите вернуться назад?", preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Выйти", style: .default) { _ in
+            self.dismiss(animated: true)
+            print("Нажата кнопка Выйти")
+        }
+        let cancelAction = UIAlertAction(title: "Отмена", style: .default) { _ in
+            print("Нажата кнопка Отмена")
+        }
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        present(alert, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
