bd3b3cea01cc7baa4ef5cb0e5e332299e1c8a1e2
Sabina Tarasova <99331891+GrafinyaDe@users.noreply.github.com>
2022-05-08 00:36:58 +0400

    анимация галереи

--- Navigation/PhotosViewController.swift
+++ Navigation/PhotosViewController.swift
@@ -48,6 +48,49 @@ class PhotosViewController: UIViewController {
         super.viewWillDisappear(animated)
         self.navigationController?.navigationBar.isHidden = true
     }
+    
+    private var backView: UIView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        //        $0.frame = view.frame
+        $0.backgroundColor = .gray
+        $0.alpha = 0.0
+        return $0
+    }(UIView())
+    
+    private var button: UIButton = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        $0.backgroundColor = .white
+        $0.setImage(UIImage(systemName: "xmark"), for: .normal)
+        $0.alpha = 0.0
+        $0.addTarget(self, action: #selector(cancelAction), for: .touchUpInside)
+        //             $0.actions(forTarget: cancelAction, forControlEvent: .touchUpInside)
+        return $0
+    }(UIButton())
+    
+    @objc private func cancelAction() {
+        UIView.animate(withDuration: 0.3, delay: 0, options: .curveEaseInOut) { [self] in
+            button.alpha = 0.0
+        } completion: { _ in
+            UIView.animate(withDuration: 0.5) { [self] in
+                
+                backView.alpha = 0.0
+                
+                image.removeFromSuperview()
+            }
+        }
+    }
+    
+    let image: UIImageView = {
+        $0.translatesAutoresizingMaskIntoConstraints = false
+        return $0
+    }(UIImageView())
+    
+    private var imgX = NSLayoutConstraint()
+    private var imgY = NSLayoutConstraint()
+    private var imgWidht = NSLayoutConstraint()
+    private var imgHeight = NSLayoutConstraint()
+    
+    
 }
 
 //MARK: UICollectionViewDataSource
@@ -67,6 +110,7 @@ extension PhotosViewController: UICollectionViewDataSource {
 //MARK: UICollectionViewDelegateFlowLayout
 
 extension PhotosViewController: UICollectionViewDelegateFlowLayout {
+    
     private var inset: CGFloat { return 8 }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
@@ -84,4 +128,40 @@ extension PhotosViewController: UICollectionViewDelegateFlowLayout {
         UIEdgeInsets(top: inset, left: inset, bottom: inset, right: inset)
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let img = photos[indexPath.item]
+        image.image = img.image
+        backView.frame = view.frame
+        
+        view.addSubview(backView)
+        view.addSubview(button)
+        view.addSubview(image)
+        
+        NSLayoutConstraint.activate([
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            image.widthAnchor.constraint(equalToConstant: view.frame.width),
+            image.heightAnchor.constraint(equalToConstant: view.frame.width)
+            
+        ])
+        
+        NSLayoutConstraint.activate([
+            button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            button.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+        ])
+        
+        UIView.animate(withDuration: 0.5, delay: 0, options: .curveEaseInOut) { [self] in
+            
+            backView.alpha = 0.7
+            
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3) { [self] in
+                button.alpha = 1
+                
+            }
+        }
+    }
 }
+
+
+
